61d2d2344822f246e6a1207b9b69c712d8d81e37
Leandro Martello <maidenleo2000@gmail.com>
2022-11-25 23:18:50 -0300

    carga de paises funcionando

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Pais } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,20 +12,23 @@ export class PorPaisComponent {
 
   termino:string = 'Honduras';
   hayError: boolean = false;
+  paises: Pais[] = [];
 
   constructor(private PaisService: PaisService) { }
 
   buscar(){
     this.hayError = false;
     console.log(this.termino);
-    this.PaisService.buscarPais(this.termino).subscribe((resp) =>{
+    this.PaisService.buscarPais(this.termino).subscribe((arr) =>{
       
-      console.log(resp);
+      console.log(arr);
+      this.paises = arr; //le paso el resultado de la busqueda directamente que es un array
       
     }, (err) => {
           console.log('Error!');
           console.info(err);
           this.hayError = true;
+          this.paises=[];
     });
     //Para que se ejecute un observable tiene que tener como mínimo un suscribe, sino no se ejecuta.
   }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
+import { Pais } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,14 @@ export class PaisService {
 
 
   //La funcion buscarPais es un método que recibe un TERMINO y retorna un OBSERVABLE
-  buscarPais(termino:string): Observable<any>{
+  buscarPais(termino:string): Observable<Pais[]>{ //aca le agrego el tipo del observable. El tipo lo tomo de la interfaz que agregue usando una respuesta de la peticion en Postman y generando la interfaz en la web https://app.quicktype.io/
     const url = `${this.apiUrl}/name/${termino}`
     // return this.http.get(url)
     // .pipe(
     //   catchError(err => of([])) //Esta funcion captura el error y retorna un arreglo vacío. Tambien puedo pasar un string para que sea devuelto si quiero entre los []
     // )
 
-    return this.http.get(url);
+    return this.http.get<Pais[]>(url); //En el GET tambien va el tipado devuelto que es el mismo que el observable de arriba
   }
 
 }

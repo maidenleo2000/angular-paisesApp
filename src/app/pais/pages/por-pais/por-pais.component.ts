4533c3df1ff7aadb2bcaa3078adbd1bdd55705e6
Leandro Martello <maidenleo2000@gmail.com>
2022-12-03 21:16:36 -0300

    Aplicacion finalizada

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,19 +6,28 @@ import { Pais } from '../../interfaces/pais.interface';
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+      li{
+        cursor:pointer;
+      }
+    `
   ]
 })
 export class PorPaisComponent {
   
   
 
-  termino:string = 'Honduras';
+  termino:string = '';
   hayError: boolean = false;  
   paises: Pais[] = [];
+
+  paisesSugeridos: Pais[] = [];
+  mostrarSugerencias:boolean = false;
 
   constructor(private PaisService: PaisService) { }
 
   buscar(termino:string){
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino; //le asigno termino a this.termino porque termino es el evento que recibo del output
     console.log(this.termino);
@@ -38,7 +47,19 @@ export class PorPaisComponent {
 
   sugerencias(termino:string){
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
     
+    this.PaisService.buscarPais(termino)
+    .subscribe(paises => this.paisesSugeridos = paises.splice(0,3),
+    (err) => this.paisesSugeridos = []);
+  }
+
+
+  buscarSugerido(termino:string){
+    this.buscar(termino);
+    
+
   }
 
 }
